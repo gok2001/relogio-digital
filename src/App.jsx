85d836332f73077aaf8964881132dd68f4082c0b
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2026-09-02 19:34:45 -0300

    feat: cria botão que remove o relógio e para o setInterval

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [hora, setHora] = useState(new Date());
+  const [mostrarRelogio, setMostrarRelogio] = useState(true);
 
   useEffect(() => {
+    if (!mostrarRelogio) {
+      return;
+    }
+
     const intervalo = setInterval(() => {
       setHora(new Date());
     }, 1000);
 
     return () => clearInterval(intervalo);
-  }, []);
+  }, [mostrarRelogio]);
 
   return (
     <div>
       <h1>Relógio Digital</h1>
-      {hora.toLocaleTimeString("pt-BR")}
+      <button onClick={() => setMostrarRelogio(!mostrarRelogio)}>{mostrarRelogio ? "Remover relógio" : "Adicionar relógio"}</button>
+
+      {mostrarRelogio && (
+        <p>{hora.toLocaleTimeString("pt-BR")}</p>
+      )}
     </div>
   );
 }
